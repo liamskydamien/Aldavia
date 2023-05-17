536f5eedadea015f91fb7d99533af00e9a30e52d
Lutz Drüke <lutz.drueke@smail.inf.h-brs.de>
2023-05-17 12:16:18 +0200

    Grundlegend StudentTest gebaut, basiert auf Kenntnistest.

--- src/test/java/org/hbrs/se2/project/aldavia/test/DatabaseTest/StudentTest.java
+++ src/test/java/org/hbrs/se2/project/aldavia/test/DatabaseTest/StudentTest.java
@@ -24,4 +24,32 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class StudentTest {
 
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public void testeRoundTrip() {
+        try {
+            Student student = new Student();
+            String id = String.valueOf(student.getUser().getId());
+            student.setVorname("Vorname");
+            student.setNachname("Nachname");
+            student.setMatrikelNummer("12348765");
+            student.setStudiengang("TestStudium");
+            student.setStudienbeginn(LocalDate.ofEpochDay(2020 - 06 - 01));
+            student.setGeburtsdatum(LocalDate.ofEpochDay(2000 - 01 - 01));
+            student.setLebenslauf("TestLebenslauf");
+            studentRepository.save(student);
+            //Saved in DB?
+            assertEquals(student, studentRepository.findByUserID(id));
+
+            //Read
+
+            //Update
+
+            //Delete
+            studentRepository.deleteByMatrikelNummer("12348765");
+        } catch (Exception e) {
+            System.out.println("Fehler bei RoundTrip: " + e.getMessage());
+        }
+    }
 }
