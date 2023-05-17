1c7fe27ca4ee3ccd1c1828b64795b1149ee34922
Liam Hess <liamskydamien@gmail.com>
2023-05-17 13:01:27 +0200

    Add SprachenRepository

---
+++ src/test/java/org/hbrs/se2/project/aldavia/test/DatabaseTest/SpracheTest.java
@@ -0,0 +1,10 @@
+package org.hbrs.se2.project.aldavia.test.DatabaseTest;
+
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class SpracheTest {
+
+}
