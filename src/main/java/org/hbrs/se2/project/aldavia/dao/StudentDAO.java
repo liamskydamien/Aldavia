3949be538a50aefeaf65a235e4947b6eb44becea
Liam Hess <liamskydamien@gmail.com>
2023-05-18 22:35:43 +0200

    Add SpracheControl Tests

--- src/main/java/org/hbrs/se2/project/aldavia/dao/StudentDAO.java
+++ src/main/java/org/hbrs/se2/project/aldavia/dao/StudentDAO.java
@@ -1,21 +1,39 @@
 package org.hbrs.se2.project.aldavia.dao;
 
+import org.hbrs.se2.project.aldavia.control.exception.PersistenceException;
 import org.hbrs.se2.project.aldavia.entities.Sprache;
 import org.hbrs.se2.project.aldavia.entities.Student;
 import org.hbrs.se2.project.aldavia.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Set;
 
+@Component
 public class StudentDAO {
 
     @Autowired
-    private StudentRepository repository;
+    private StudentRepository studentRepository;
 
-    public Student addSprache(Student student, Sprache sprache) {
+    @Autowired
+    private SpracheDAO spracheDAO;
+
+    public Student addSprache(Student student, Sprache sprache) throws PersistenceException {
         List<Sprache> sprachen = student.getSprachen();
         sprachen.add(sprache);
-        return null;
+        student.setSprachen(sprachen);
+        studentRepository.save(student);
+        spracheDAO.addStudentToSprache(student, sprache.getSpracheId());
+        return student;
+    }
+
+    public Student removeSprache(Student student, Sprache sprache) throws PersistenceException {
+        List<Sprache> sprachen = student.getSprachen();
+        sprachen.remove(sprache);
+        student.setSprachen(sprachen);
+        studentRepository.save(student);
+        spracheDAO.removeStudentFromSprache(student, sprache.getSpracheId());
+        return student;
     }
 }
