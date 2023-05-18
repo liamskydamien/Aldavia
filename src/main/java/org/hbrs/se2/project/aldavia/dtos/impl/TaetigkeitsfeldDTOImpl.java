38c8dca3f9bb064400ab7ffe274f520248a7af92
Liam Hess <liamskydamien@gmail.com>
2023-05-18 17:13:25 +0200

    Add more DTOs

---
+++ src/main/java/org/hbrs/se2/project/aldavia/dtos/impl/TaetigkeitsfeldDTOImpl.java
@@ -0,0 +1,11 @@
+package org.hbrs.se2.project.aldavia.dtos.impl;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hbrs.se2.project.aldavia.dtos.TaetigkeitsfeldDTO;
+
+@Getter
+@Setter
+public class TaetigkeitsfeldDTOImpl implements TaetigkeitsfeldDTO {
+    private String bezeichnung;
+}
