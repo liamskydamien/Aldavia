5c20d633e520dfbd386cb7006f710e71bff14a98
Liam Hess <liamskydamien@gmail.com>
2023-05-20 18:56:42 +0200

    Added AL for StudentProfile

--- src/main/java/org/hbrs/se2/project/aldavia/views/StudentProfileView.java
+++ src/main/java/org/hbrs/se2/project/aldavia/views/StudentProfileView.java
@@ -1,53 +1,48 @@
 package org.hbrs.se2.project.aldavia.views;
 
-import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.router.*;
 import org.hbrs.se2.project.aldavia.control.StudentProfileControl;
-import org.hbrs.se2.project.aldavia.util.Globals;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.hbrs.se2.project.aldavia.control.exception.ProfileException;
+import org.hbrs.se2.project.aldavia.dtos.StudentProfileDTO;
 
 import java.util.List;
 import java.util.Map;
 
-@Route(value = Globals.Pages.PROFILE_VIEW, layout = LoggedInStateLayout.class)
-@PageTitle("Profil")
-public class StudentProfileView extends Div implements HasUrlParameter<String> {
+@Route(value = "profile/student", layout = NeutralLayout.class)
+public class StudentProfileView extends Div implements HasUrlParameter<String>, HasDynamicTitle {
 
-    @Autowired
-    private StudentProfileControl studentProfileControl;
-    private H1 title = new H1("Profil");
+    private final StudentProfileControl studentProfileControl;
+
+    private String title = "";
+
+    private StudentProfileDTO studentProfileDTO;
+
+    public StudentProfileView(StudentProfileControl studentProfileControl) {
+        this.studentProfileControl = studentProfileControl;
+    }
+    @Override
+    public String getPageTitle() {
+        return title;
+    }
+
     @Override
     public void setParameter(BeforeEvent event,
-                             @OptionalParameter String parameter) {
-        Location location = event.getLocation();
-        QueryParameters queryParameters = location
-                .getQueryParameters();
-
-        Map<String, List<String>> parametersMap = queryParameters.getParameters();
-        List<String> usernames = parametersMap.get("username");
-        if(usernames != null && !usernames.isEmpty()){
-            addTextToView(usernames.get(0));
-        } else {
-            // Handle the case where there is no "username" parameter or the list is empty
-            add(new Text("Fehler beim Laden des Profils"));
-        }
-    }
-    public StudentProfileView(StudentProfileControl studentProfileControl) {
-        addClassName("profile-view");
-        add(title);
-    }
-
-    public void addTextToView(String text) {
-        try{
-        add(new Text(studentProfileControl.getStudentProfile(text).toString()));
-        }catch (Exception e){
-            add(new Text("Fehler beim Laden des Profils"));
-        }
+                @OptionalParameter String parameter) {
+            Location location = event.getLocation();
+            QueryParameters queryParameters = location
+                    .getQueryParameters();
+            Map<String, List<String>> parametersMap = queryParameters.getParameters();
+            try {
+                studentProfileDTO = studentProfileControl.getStudentProfile(parametersMap.get("username").get(0));
+                title = studentProfileDTO.getVorname() + " " + studentProfileDTO.getNachname();
+            }
+            catch (ProfileException e) {
+                e.printStackTrace();
+            }
     }
 
 
-    // TEST TEST
+
 
 }
---
+++ src/test/java/org/hbrs/se2/project/aldavia/test/ProfileTest/TesteTaetigkeitsfeldDAO.java
@@ -0,0 +1,78 @@
+package org.hbrs.se2.project.aldavia.test.ProfileTest;
+
+import org.hbrs.se2.project.aldavia.control.exception.PersistenceException;
+import org.hbrs.se2.project.aldavia.dao.TaetigkeitsfeldDAO;
+import org.hbrs.se2.project.aldavia.dtos.impl.TaetigkeitsfeldDTOImpl;
+import org.hbrs.se2.project.aldavia.entities.Student;
+import org.hbrs.se2.project.aldavia.entities.Taetigkeitsfeld;
+import org.hbrs.se2.project.aldavia.repository.TaetigkeitsfeldRepository;
+import org.hbrs.se2.project.aldavia.test.TestStudentFactory;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Transactional
+public class TesteTaetigkeitsfeldDAO {
+    @Autowired
+    private TaetigkeitsfeldDAO taetigkeitsfeldDAO;
+
+    @Autowired
+    private TestStudentFactory testStudentFactory;
+
+    @Autowired
+    private TaetigkeitsfeldRepository taetigkeitsfeldRepository;
+
+    @Test
+    public void testAddTaetigkeitsfeld(){
+        TaetigkeitsfeldDTOImpl taetigkeitsfeld = new TaetigkeitsfeldDTOImpl();
+        taetigkeitsfeld.setBezeichnung("Test");
+        try{
+            Taetigkeitsfeld testTaetigkeitsfeld = taetigkeitsfeldDAO.addTaetigkeitsfeld(taetigkeitsfeld);
+            assertEquals(testTaetigkeitsfeld.getBezeichnung(), taetigkeitsfeld.getBezeichnung());
+            assertTrue(taetigkeitsfeldRepository.findById(testTaetigkeitsfeld.getBezeichnung()).isPresent());
+            taetigkeitsfeldRepository.deleteById(testTaetigkeitsfeld.getBezeichnung());
+        }
+        catch (PersistenceException e){
+            System.out.println("Fehler");
+        }
+    }
+
+    @Test
+    public void testDeleteTaetigkeitsfeld(){
+        TaetigkeitsfeldDTOImpl taetigkeitsfeld = new TaetigkeitsfeldDTOImpl();
+        taetigkeitsfeld.setBezeichnung("Test");
+        try{
+            Taetigkeitsfeld testTaetigkeitsfeld = taetigkeitsfeldDAO.addTaetigkeitsfeld(taetigkeitsfeld);
+            taetigkeitsfeldDAO.deleteTaetigkeitsfeld(testTaetigkeitsfeld);
+            assertFalse(taetigkeitsfeldRepository.findById(testTaetigkeitsfeld.getBezeichnung()).isPresent());
+        }
+        catch (PersistenceException e){
+            System.out.println("Fehler");
+        }
+    }
+
+    @Test
+    public void testAddAndDeleteStudent(){
+        TaetigkeitsfeldDTOImpl taetigkeitsfeldDTO = new TaetigkeitsfeldDTOImpl();
+        taetigkeitsfeldDTO.setBezeichnung("Test");
+        Student student = testStudentFactory.createStudent();
+        try{
+            Taetigkeitsfeld taetigkeitsfeld = taetigkeitsfeldDAO.addTaetigkeitsfeld(taetigkeitsfeldDTO);
+            taetigkeitsfeldDAO.addStudentToTaetigkeitsfeld(student, taetigkeitsfeld);
+            assertTrue(taetigkeitsfeld.getStudenten().contains(student));
+            taetigkeitsfeldDAO.removeStudentFromTaetigkeitsfeld(student, taetigkeitsfeld);
+            assertFalse(taetigkeitsfeld.getStudenten().contains(student));
+            taetigkeitsfeldDAO.deleteTaetigkeitsfeld(taetigkeitsfeld);
+            assertFalse(taetigkeitsfeldRepository.findById(taetigkeitsfeld.getBezeichnung()).isPresent());
+        }
+        catch (PersistenceException e){
+            System.out.println("Fehler");
+        }
+    }
+}
