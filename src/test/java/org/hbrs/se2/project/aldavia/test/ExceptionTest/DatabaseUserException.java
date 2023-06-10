3d211834e7de81c1dd3061ecd75fdf0fb72b16d2
Lutz Drüke <lutz.drueke@smail.inf.h-brs.de>
2023-06-10 11:49:31 +0200

    ExceptionTests vorbereitet.

---
+++ src/test/java/org/hbrs/se2/project/aldavia/test/ExceptionTest/DatabaseUserException.java
@@ -0,0 +1,17 @@
+package org.hbrs.se2.project.aldavia.test.ExceptionTest;
+
+import org.hbrs.se2.project.aldavia.control.exception.DatabaseUserException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+
+@SpringBootTest
+public class DatabaseUserException {
+}
---
+++ src/test/java/org/hbrs/se2/project/aldavia/test/ExceptionTest/ProfileException.java
@@ -0,0 +1,4 @@
+package org.hbrs.se2.project.aldavia.test.ExceptionTest;
+
+public class ProfileException {
+}
