220c1f837574931b3ce00027fc04af4277dd844b
Liam Hess <liamskydamien@gmail.com>
2023-06-10 23:44:29 +0200

    Add Empfhelungssort + Comparator + Test for Comparator

--- src/main/java/org/hbrs/se2/project/aldavia/control/SearchControl.java
+++ src/main/java/org/hbrs/se2/project/aldavia/control/SearchControl.java
@@ -2,15 +2,20 @@ package org.hbrs.se2.project.aldavia.control;
 
 import org.hbrs.se2.project.aldavia.control.factories.StellenanzeigeDTOFactory;
 import org.hbrs.se2.project.aldavia.dtos.StellenanzeigeDTO;
+import org.hbrs.se2.project.aldavia.entities.Kenntnis;
 import org.hbrs.se2.project.aldavia.entities.Stellenanzeige;
+import org.hbrs.se2.project.aldavia.entities.Student;
+import org.hbrs.se2.project.aldavia.entities.Taetigkeitsfeld;
 import org.hbrs.se2.project.aldavia.service.StellenanzeigenService;
 import org.hbrs.se2.project.aldavia.service.StudentService;
+import org.hbrs.se2.project.aldavia.util.comperators.StellenanzeigenComparator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -34,6 +39,7 @@ public class SearchControl {
 
     /**
      * Returns all Stellenanzeigen
+     *
      * @return List of StellenanzeigeDTOs
      */
     public List<StellenanzeigeDTO> getAllStellenanzeigen() {
@@ -45,8 +51,31 @@ public class SearchControl {
         return stellenanzeigenDTO;
     }
 
-
-
-
+    /**
+     * Sorts the outputted List based on the users preferences
+     * @param username Username of the student
+     * @return  a list sorted on the users preferences
+     */
+    public List<StellenanzeigeDTO> getRecommendedStellenanzeigen(String username) {
+        logger.info("Collecting recommended Stellenanzeigen for user {}", username);
+        logger.info("Collecting information about user {}", username);
+        try {
+            Student student = studentService.getStudent(username);
+            logger.info("Found user {}", username);
+            logger.info("Collecting all information about {} interests and skills", username);
+            List<String> interessen = student.getTaetigkeitsfelder().stream().map(Taetigkeitsfeld::getBezeichnung).collect(Collectors.toList());
+            List<String> kenntnisse = student.getKenntnisse().stream().map(Kenntnis::getBezeichnung).collect(Collectors.toList());
+            logger.info("Collected needed information.");
+            StellenanzeigenComparator stellenanzeigenComparator = new StellenanzeigenComparator(interessen, kenntnisse);
+            List<StellenanzeigeDTO> stellenanzeigenDTOs = getAllStellenanzeigen();
+            logger.info("Sorting Stellenanzeigen based on user");
+            stellenanzeigenDTOs.sort(stellenanzeigenComparator);
+            logger.info("Sorted Stellenanzeigen accordingly.");
+            return stellenanzeigenDTOs;
+        } catch (Exception e) {
+            logger.error("Could not find user {}", username);
+            return getAllStellenanzeigen();
+        }
+    }
 
 }
