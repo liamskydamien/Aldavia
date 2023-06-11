c782a2e144ca22e1c11abfabbd5a555fdf74c5da
Lutz Drüke <lutz.drueke@smail.inf.h-brs.de>
2023-06-11 14:50:40 +0200

    Test testUserNotFound hat noch probleme mit dem Repository

--- src/test/java/org/hbrs/se2/project/aldavia/test/ExceptionTest/DatabaseUserExceptionTest.java
+++ src/test/java/org/hbrs/se2/project/aldavia/test/ExceptionTest/DatabaseUserExceptionTest.java
@@ -2,16 +2,25 @@ package org.hbrs.se2.project.aldavia.test.ExceptionTest;
 
 import org.hbrs.se2.project.aldavia.control.exception.DatabaseUserException;
 import org.hbrs.se2.project.aldavia.control.LoginControl;
-import org.junit.jupiter.api.AfterEach;
+import org.hbrs.se2.project.aldavia.entities.User;
+import org.hbrs.se2.project.aldavia.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.dao.DataAccessResourceFailureException;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 
 @SpringBootTest
@@ -22,7 +31,14 @@ public class DatabaseUserExceptionTest {
         DatabaseUserException exception = new DatabaseUserException(DatabaseUserException.DatabaseUserExceptionType.USER_NOT_FOUND, "User not found");
         Assertions.assertEquals(DatabaseUserException.DatabaseUserExceptionType.USER_NOT_FOUND, exception.getDatabaseUserExceptionType());
     }
-    //Test ob DatabaseUserExceptionType so funktioniert wie es soll
+    //Test ob User-Not-Found so funktioniert wie es soll
+
+    @Test
+    void testGetDatabaseUserExceptionType2() {
+        DatabaseUserException exception2 = new DatabaseUserException(DatabaseUserException.DatabaseUserExceptionType.DATABASE_CONNECTION_FAILED, "A failure occured while trying to connect to database. Please try again later.");
+        Assertions.assertEquals(DatabaseUserException.DatabaseUserExceptionType.DATABASE_CONNECTION_FAILED, exception2.getDatabaseUserExceptionType());
+    }
+    //Test ob Database-Connection-Failed so funktioniert wie es soll
 
     @Test
     void testGetReason() {
@@ -32,25 +48,23 @@ public class DatabaseUserExceptionTest {
     }
     //Test ob GetReason funktioniert wie es funktionieren soll
 
-    private LoginControl loginControl;  //Es folgt Exceptiontest mit Login-Controll
+    private LoginControl loginControl;
+    private UserRepository userRepository;
 
     @BeforeEach
     void setUp() {
         loginControl = new LoginControl();
+        //UserRepository repository = new UserRepository();
     }
 
-    /*
     @Test
-    void testAuthenticateUserNotFound() {
-        String username = "nonexistentUser";
-        String password = "password123";
+    void testUserNotFound(){
+        String username = "gibtsNicht";
+        String password = "password1234";
 
-        DatabaseUserException exception = Assertions.assertThrows(DatabaseUserException.class, () ->
-                loginControl.authenticate(username, password));
+        DatabaseUserException ex = assertThrows(DatabaseUserException.class, () -> loginControl.authenticate(username, password));
 
-        Assertions.assertEquals(DatabaseUserException.DatabaseUserExceptionType.USER_NOT_FOUND, exception.getDatabaseUserExceptionType());
-        Assertions.assertEquals("No User could be found! Please check your credentials!", exception.getMessage());
+        Assertions.assertEquals(DatabaseUserException.DatabaseUserExceptionType.USER_NOT_FOUND, ex.getDatabaseUserExceptionType());
+        Assertions.assertEquals("No User could be found! Please check your credentials!", ex.getMessage());
     }
-
-     */
 }
