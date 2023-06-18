32524d8cf95e95e47c3a67b4f9423feb996651ae
Liam Hess <liamskydamien@gmail.com>
2023-06-18 18:28:31 +0200

    Add BewerbungsCreation View

--- src/main/java/org/hbrs/se2/project/aldavia/dtos/BewerbungsDTO.java
+++ src/main/java/org/hbrs/se2/project/aldavia/dtos/BewerbungsDTO.java
@@ -15,4 +15,8 @@ public class BewerbungsDTO {
     private StellenanzeigeDTO stellenanzeige;
     private String status;
     private String bewerbungsSchreiben;
+
+    public void setBewerbungsSchreiben(String bewerbungsSchreiben) {
+        this.bewerbungsSchreiben = bewerbungsSchreiben;
+    }
 }
