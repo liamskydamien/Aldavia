1396a33b43ad73746fe9c557288372d58893b5de
Liam Hess <liamskydamien@gmail.com>
2023-06-24 10:16:43 +0200

    Updated StellenanzeigenServiceTest

--- src/test/java/org/hbrs/se2/project/aldavia/test/StellenanzeigenSuchenTest/TaetigkeitsfeldStellenanzeigenTest.java
+++ src/test/java/org/hbrs/se2/project/aldavia/test/StellenanzeigenSuchenTest/TaetigkeitsfeldStellenanzeigenTest.java
@@ -1,4 +1,30 @@
 package org.hbrs.se2.project.aldavia.test.StellenanzeigenSuchenTest;
 
+import org.hbrs.se2.project.aldavia.repository.TaetigkeitsfeldRepository;
+import org.hbrs.se2.project.aldavia.service.StellenanzeigenService;
+import org.hbrs.se2.project.aldavia.service.TaetigkeitsfeldService;
+import org.hbrs.se2.project.aldavia.service.UnternehmenService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class TaetigkeitsfeldStellenanzeigenTest {
+    @Autowired
+    private TaetigkeitsfeldRepository taetigkeitsfeldRepository;
+
+    @Autowired
+    private TaetigkeitsfeldService taetigkeitsfeldService;
+
+    @Autowired
+    private UnternehmenService unternehmenService;
+
+    @Autowired
+    private StellenanzeigenService stellenanzeigeService;
+
+
+    @Test
+    public void testTaetigkeitsfeldStellenanzeigen() {
+
+    }
 }
